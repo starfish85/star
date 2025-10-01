b081d670ea7eb9165dc9f7ab28160ef511039e21
starfish85 <3448874538@qq.com>
2025-10-01 16:24:46 +0800

    Add files via upload

---
+++ guess.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+#include <ctime>
+int main() {
+	srand((unsigned int)time(NULL));
+	int num=rand() % 100+1;
+	int val = 0;
+	while (1) {
+		cin >> val;
+		if (val > num) {
+			cout << "数字偏大" << endl;
+
+		}
+		else if (val < num) { cout << "数字偏小" << endl; }
+		else {
+			cout << "猜对了" << endl;
+			break;
+		}
+	}
+	
+
+
+	system("pause");
+	return 0;
+}
---
+++ weight  of pig.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int A = 0;
+	int B = 0;
+	int C = 0;
+	cout << "请输入小猪A体重" << endl;
+	cin >> A;
+	cout << "请输入小猪B体重" << endl;
+	cin >> B;
+    cout << "请输入小猪C体重" << endl;
+	cin >> C;
+
+
+	
+	cout << "小猪A体重为：" << A << endl;
+	cout << "小猪B体重为：" << B << endl;
+	cout << "小猪C体重为：" << C << endl;
+
+	if (A > B) {
+		if (A > C) { cout << "A最重" << endl; }
+		else {
+			cout << "C最重" << endl;
+		}
+	}
+		else if(A>C) {if(A>B){ cout << "A最重" << endl; }
+		else {
+		cout << "B最重" << endl;
+	}
+
+		}
+		else if (B > A) { if (B >C) { cout << "B最重" << endl; }
+		else { cout << "C最重" << endl; }
+	}
+		else {
+		if (B > A) { cout << "B最重" << endl; }
+		else {
+			cout << "A最重" << endl;
+		}
+	}
+		
+
+	
+	
+	system("pause");
+	return 0;
+}
