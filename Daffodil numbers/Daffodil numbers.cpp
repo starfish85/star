014e83559c74841a5953dc483cbbdff0d4294ab5
starfish85 <3448874538@qq.com>
2025-10-02 14:51:16 +0800

    Add files via upload

---
+++ Daffodil numbers/Daffodil numbers.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int num = 100;
+	
+	do {
+		int a = 0;
+		int b = 0;
+		int c = 0;
+		a = num % 10;
+		b = num / 10 % 10;
+		c = num / 100;
+		if (num == a * a * a + b * b * b + c * c * c) {
+			cout << num << endl;
+		}
+			num++;
+		
+	} while (num < 10000);
+
+	system("pause");
+	return 0;
+}
